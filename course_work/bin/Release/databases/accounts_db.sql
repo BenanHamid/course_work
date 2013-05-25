d2548363be4c5cac4e64128fc5ef4a959846218d
detox <klegardoz@abv.bg>
2013-05-25 11:53:40 +0300

    [0106] Clean up .SQL files. Да са начисто.

--- course_work/bin/Release/databases/accounts_db.sql
+++
@@ -1,6 +0,0 @@
-abv	abv
-abv	abv
-agv	agv
-asdf	123
-123a	123
-admin	password
--- course_work/bin/Release/databases/orders_db.sql
+++ course_work/bin/Release/databases/orders_db.sql
@@ -1,2 +1,1 @@
-dasdsa	dasd	Юридическо лице	dasd	Пещера	dasdas	dad	Наложен платеж
-pe6ter	rretet	Юридическо лице	dsad	Козлодуй	das	das	Наложен платеж
+﻿
